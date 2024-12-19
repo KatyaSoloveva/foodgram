3beaa39abfe296a7f5caa71e629de9c28b8b98de
Ekaterina Soloveva <KkatSsol@yandex.ru>
2024-12-19 16:59:05 +0300

    Изменен файл load_data

--- backend/foodgram/recipes/management/commands/load_data.py
+++ backend/foodgram/recipes/management/commands/load_data.py
@@ -9,11 +9,21 @@ from recipes.models import Ingredient
 class Command(BaseCommand):
     def handle(self, *args, **options):
         file_path = settings.BASE_DIR / 'data/ingredients.csv'
-        with open(file_path, 'r', encoding='utf-8') as f:
-            reader = csv.reader(f)
-            for row in reader:
-                name, measurement_unit = row
-                ingredient, _ = Ingredient.objects.get_or_create(
-                    name=name,
-                    measurement_unit=measurement_unit)
-        self.stdout.write(self.style.SUCCESS('Ингредиенты добавлены.'))
+        try:
+            with open(file_path, 'r', encoding='utf-8') as f:
+                reader = csv.reader(f)
+                ingredients = []
+                for row in reader:
+                    name, measurement_unit = row
+                    ingredients.append(
+                        Ingredient(
+                            name=name,
+                            measurement_unit=measurement_unit
+                        )
+                    )
+                Ingredient.objects.bulk_create(
+                    ingredients, ignore_conflicts=True
+                )
+            self.stdout.write(self.style.SUCCESS('Ингредиенты добавлены.'))
+        except FileNotFoundError:
+            self.stdout.write(self.style.ERROR('Проблемы с открытием файла.'))
